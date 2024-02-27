aed91d69bac5aa62def6214cdd24cf03ac7942e8
Robson H. Rodrigues <robhenrod@gmail.com>
2024-02-27 15:37:52 -0300

    Desacoplando camadas no NestJS - Copiando camada de Domínio

---
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -0,0 +1,52 @@
+import { Either, right } from '@/core/either'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface AnswerQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  content: string
+  attachmentsIds: string[]
+}
+
+type AnswerQuestionUseCaseResponse = Either<
+  null,
+  {
+    answer: Answer
+  }
+>
+
+export class AnswerQuestionUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    authorId,
+    questionId,
+    content,
+    attachmentsIds,
+  }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
+    const answer = Answer.create({
+      authorId: new UniqueEntityID(authorId),
+      questionId: new UniqueEntityID(questionId),
+      content,
+    })
+
+    const answerAttachments = attachmentsIds.map((attachmentId) => {
+      return AnswerAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        answerId: answer.id,
+      })
+    })
+
+    answer.attachments = new AnswerAttachmentList(answerAttachments)
+
+    await this.answersRepository.create(answer)
+
+    return right({
+      answer,
+    })
+  }
+}
---
+++ src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -0,0 +1,50 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from '@/core/errors/not-allowed-error'
+import { makeNotification } from 'test/factories/make-notification'
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { ReadNotificationUseCase } from './read-notification'
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sut: ReadNotificationUseCase // Subject Under Test
+
+describe('Read Notification', () => {
+  beforeEach(() => {
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    sut = new ReadNotificationUseCase(inMemoryNotificationsRepository) // Subject Under Test
+  })
+
+  it('should be able to read a notification', async () => {
+    const notification = makeNotification()
+
+    await inMemoryNotificationsRepository.create(notification)
+
+    const result = await sut.execute({
+      recipientId: notification.recipientId.toString(),
+      notificationId: notification.id.toString(),
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryNotificationsRepository.items[0].readAt).toEqual(
+      expect.any(Date),
+    )
+  })
+
+  it('should not be able to read a notification from another user', async () => {
+    const newNotification = makeNotification(
+      {
+        recipientId: new UniqueEntityID('recipient-1'),
+      },
+      new UniqueEntityID('notification-1'),
+    )
+
+    await inMemoryNotificationsRepository.create(newNotification)
+
+    const result = await sut.execute({
+      notificationId: 'notification-1',
+      recipientId: 'recipient-2',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+})
